e534cb8782372aa639a6c36fb3621fdc893f82a1
yerim Jeong <dnaer456@gmail.com>
2025-09-22 00:24:20 +0900

    중첩 리스트

--- Day2/python.py
+++ Day2/python.py
@@ -39,3 +39,34 @@ a = " Happy "
 print(a.lstrip())
 print(a.rstrip())
 print(a.strip())
+
+# replace() - 문자열 바꾸기
+# replace(바뀔 문자열, 바꿀 문자열)
+
+a = "happy"
+a.replace("y", "end")
+
+# split() - 문자열 나누기
+# split() 기본 설정은 공백 기준, 반환되는 값은 리스트 자료형으로 반환
+a = "Happy1Happy2Happy3"
+a.split("Happy1Happy2Happy3")
+
+# 리스트 자료형 = 배열
+# 하나의 변수에 여러 개의 값을 저장, 인덱스로 접근
+# 대괄호로 감싸주고 요소값은 , 로 구분
+
+a = []
+a = list()
+b = [1,2,3]
+c = ["Happy", 2025]
+print(a)
+print(b)
+print(c, type(c), type(c[0]), type(c[1]))
+
+# 중첩 리스트 - 배열과 같은 문법으로 중첩 리스트 사용이 가능
+a = [1,2,3,["a","b","c"]]
+print(a[0])
+print(a[3])
+
+# 2차열 배열
+print(a[3][2])
