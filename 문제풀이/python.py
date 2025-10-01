8ec9831d3578c99cdaf05da0f57250ddf16d6e27
yerim Jeong <dnaer456@gmail.com>
2025-10-02 00:35:06 +0900

    등차수열

--- 문제풀이/python.py
+++ 문제풀이/python.py
@@ -7,6 +7,17 @@ def solution1(a, b):
 def solution2(a, b):
  return max(int(str(a) + str(b)), 2 * a * b)
 
- #flag에 따라 다른 값 반환하기
+#flag에 따라 다른 값 반환하기
 def solution3(a, b, flag):
    return  a + b if flag else a - b
+
+# 등차수열
+a,d, included = 3, 4, [True, False, False, True, True]
+print(a, d, included)
+
+def solution4(a, d, included):
+   answer = 0
+   for i in range(len(included)):
+      print(i, included[i], int(included[i]))
+   answer += (a + i * d) * included[i]
+   return answer
