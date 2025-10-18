faf4b38634a24d06bc3e638ce87ebc117fa986c4
yerim Jeong <dnaer456@gmail.com>
2025-10-19 00:11:36 +0900

    람다식

--- Day4/python2.py
+++ Day4/python2.py
@@ -22,3 +22,21 @@ example = "computer.mokwon.edu"
 example.split(".")
 a,b,c = example.split(".")
 print(a,b,c)
+
+f = lambda x,y: x + y
+f1 = f(1,5)
+print(f1)
+
+f = lambda x: x**2
+f2 = f(3)
+print(f2)
+
+f = lambda x: x/2
+f3 = f(3)
+print(f3)
+
+f = open("dream.txt","r")
+contents = f.read()
+print(contents)
+f.close()
+
