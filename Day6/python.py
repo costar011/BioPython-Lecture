768a611c9c47ff7c99e09c8e34ccaaf859a41542
yerim Jeong <dnaer456@gmail.com>
2025-11-13 16:11:03 +0900

    문제 풀이

--- Day6/python.py
+++ Day6/python.py
@@ -3,4 +3,10 @@ def solution(n_str):
 
 def solution(num_list):
     return max(sum(num_list[i] for i in range(0, len(num_list), 2)),
-               sum(num_list[i] for i in range(1, len(num_list), 2)))
+            sum(num_list[i] for i in range(1, len(num_list), 2)))
+
+def solution(rny_string):
+    return rny_string.replace('m', 'rn')
+
+def solution(myString, pat):
+    return int(pat.lower() in myString.lower())
