fcb231166fc2fff63ae71e7600f68bdbb6f5e6b2
yerim Jeong <dnaer456@gmail.com>
2025-11-22 01:03:17 +0900

    argparse

--- day7/python.py
+++ day7/python.py
@@ -1,11 +1,15 @@
-parser.add_argument("name", help='사용자 이름')
-parser.add_argument('--age', type=int, default=20, help='사용자 이름')
+import argparse
+
+parser = argparse.ArgumentParser(description='argparse 기초 예제')
+parser.add_argument('name', help='사용자 이름')
+parser.add_argument('--age', type=int, default=20, help='사용자 나이')
 parser.add_argument('-y', '--yes', action='store_true', help='확인 플래그')
+parser.add_argument('-f', '--format', default='json', choices=['json', 'xml', 'csv'], help='출력 형식 선택')
 
 args = parser.parse_args()
 
-print(f"Hello, {args.name}")
+print(f"Hello, {args.name}!")
 print(f"Age is set to: {args.age}")
 
 if args.yes:
-    print("Confirmed")
+    print("Confirmed!")
